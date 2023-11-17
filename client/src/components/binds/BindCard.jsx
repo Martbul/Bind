87483f3bda0bc8d4390f4c71095044ff0aabfcc2
Martbul <martbul01@gmail.com>
2023-11-17 23:11:02 +0200

    add modal to bindCard
    you need to make the UI better

--- client/src/components/binds/BindCard.jsx
+++ client/src/components/binds/BindCard.jsx
@@ -1,18 +1,36 @@
 import Card from "react-bootstrap/Card";
-
+import InfoModal from "./InfoModal";
+import { useEffect, useState } from "react";
 function BindCard({ userFullName, userAddress, dayForDelivery, user_id }) {
+
+
+   const [showMoreInfoModal, setMoreInfoModal] = useState(false);
+
+  
+   const createMoreInfoModal = () => {
+     setMoreInfoModal(true);
+   };
+
+   const hideMoreInfoModal = () => {
+     setMoreInfoModal(false);
+   };
+  
   return (
+
+    <>
+    
+    {showMoreInfoModal && <InfoModal hideModal={hideMoreInfoModal} />}
     <Card style={{ width: "18rem" }}>
       <Card.Body>
-        <Card.Title>{ userFullName}</Card.Title>
+        <Card.Title>{userFullName}</Card.Title>
         <Card.Subtitle className="mb-2 text-muted">{userAddress}</Card.Subtitle>
-        <Card.Text>
-          {dayForDelivery}
-        </Card.Text>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
+          <Card.Text>{dayForDelivery}</Card.Text>
+          <button onClick={createMoreInfoModal}>Show more info</button>
+       
       </Card.Body>
     </Card>
+    </>
+      
   );
 }
 
--- client/src/components/binds/BindsGrid.jsx
+++ client/src/components/binds/BindsGrid.jsx
@@ -2,8 +2,10 @@ import { useEffect, useState } from "react";
 import * as bindsService from '../../services/bindsService'
 import BindCard from "./BindCard"
 
+
 export default function BindsGrid() {
    const [binds, setBinds] = useState([])
+   
 
    useEffect(() => {
      bindsService.getAll()
@@ -11,20 +13,23 @@ export default function BindsGrid() {
        .catch((err) => console.log(err));
    }, []);
 
-   return (
 
-      <>
-      
-         {binds.map(bind => (
-            <BindCard
-               key={bind._id}
-               user_id={ bind._id}
-               userFullName={bind.fullname}
-               userAddress={bind.address}
-               dayForDelivery={bind.dayForDelivery}
-            />
-         ))}
-      </>
-    
-   )
+   
+
+
+   return (
+     <>
+     
+
+       {binds.map((bind) => (
+         <BindCard
+           key={bind._id}
+           user_id={bind._id}
+           userFullName={bind.fullname}
+           userAddress={bind.address}
+           dayForDelivery={bind.dayForDelivery}
+         />
+       ))}
+     </>
+   );
 }
