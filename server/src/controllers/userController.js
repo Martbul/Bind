09ca96f5bc8022ad57240b1985aa8a766c8b0db4
Martbul <martbul01@gmail.com>
2024-01-09 20:53:08 +0200

    nothing4

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -9,67 +9,58 @@ const { extractErrorMsgs } = require("../utils/errorHandler");
 //const isStrongPassword = require("validator/lib/isStrongPassword");
 //const isEmail = require("validator/lib/isEmail");
 
-
-
-
 router.post("/singup", async (req, res) => {
   const { username, email, password } = req.body;
   //console.log(req.body.username);
 
   try {
-    const decodedToken = await userService.singup({ username,email, password });
-    //! u need to make jwt work, but for the SFPROJECT DEFENCE I WILL USE AS A RESULT THE GIVEN 
+    const decodedToken = await userService.singup({
+      username,
+      email,
+      password,
+    });
+    //! u need to make jwt work, but for the SFPROJECT DEFENCE I WILL USE AS A RESULT THE GIVEN
     //!USERNAME AND EMAIL WITHOUT USING THE RESULT
-  //  const decodedUsername = jwtDecode(`'${result}'`)
+    //  const decodedUsername = jwtDecode(`'${result}'`)
     //console.log(username1);
-//console.log(result);
+    //console.log(result);
     //let decodedToken = decoder(result)
     //console.log('DECODED TOKEN   ' + decodedToken);
-   // res.cookie("auth", token, { httpOnly: true });
-   res.json(
-    decodedToken
-   // username,email
-   );
+    // res.cookie("auth", token, { httpOnly: true });
+    res.json(
+      decodedToken
+      // username,email
+    );
   } catch (error) {
     const errorMessages = extractErrorMsgs(error);
     console.log(errorMessages);
-    return errorMessages
-   // res.status(404).render("singUp", { errorMessages });
+    return errorMessages;
+    // res.status(404).render("singUp", { errorMessages });
   }
 });
-
 
 router.post("/login", async (req, res) => {
-  
   const { email, password } = req.body;
   try {
-    const decodedToken = await userService.login(  email, password );
+    const decodedToken = await userService.login(email, password);
 
-   // const decoded = await jwt.decode(result, SECRET);
+    // const decoded = await jwt.decode(result, SECRET);
     // let decodedToken = decoder(result)
-  //  console.log('DECODED TOKEN   ' + decoded.username);
+    //  console.log('DECODED TOKEN   ' + decoded.username);
     // console.log("TOKEN111111111" + token);
-  
+
     //res.cookie("auth", result, { httpOnly: true });
-      res.json(decodedToken)
-    
+    res.json(decodedToken);
   } catch (error) {
     const errorMessages = extractErrorMsgs(error);
-   return Error
+    return Error;
   }
 });
-
-
 
 router.get("/logout", (req, res) => {
   res.clearCookie("auth");
   res.clearCookie("authDeliver");
   res.redirect("/");
 });
-
-
-
-
-
 
 module.exports = router;
